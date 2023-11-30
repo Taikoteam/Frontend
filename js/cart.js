dc1e168bffca1be67b3d21576ff8710fd95e419b
Gilberto André García Gaytán <a01753176@itesm.mx>
2023-11-30 09:43:06 -0600

    Front

---
+++ js/cart.js
@@ -0,0 +1,48 @@
+const CartItems = document.querySelector(".cart-items");
+const CartTotal = document.querySelector(".cart-total");
+
+function displayCartItems() {
+  const items = JSON.parse(localStorage.getItem("cart")) || [];
+  console.log(items)
+
+  let cartTotal = 0; // Inicializar el total del carrito
+
+  items.forEach((item) => {
+    const cartItem = document.createElement("div");
+    console.log(item)
+    cartItem.className = "cart_item";
+    cartItem.innerHTML = `
+    
+      <p class="cart_title">${item.title}</p>
+      <p class="cart_price">$${item.price}</p>
+      <p class="cart_delete">Delete</p>
+    `;
+    CartItems.appendChild(cartItem);
+
+    // Sumar el precio del artículo al total del carrito
+    cartTotal += parseFloat(item.price);
+  });
+
+  // Mostrar el total del carrito
+  CartTotal.innerHTML = `Total: $${cartTotal.toFixed(2)}`;
+
+  // Agregar evento de clic para eliminar el elemento del carrito
+  const deleteButtons = document.querySelectorAll(".cart_delete");
+  deleteButtons.forEach((button, index) => {
+    button.addEventListener("click", () => handleDeleteItem(index));
+  });
+}
+
+function handleDeleteItem(index) {
+  let items = JSON.parse(localStorage.getItem("cart")) || [];
+
+  // Eliminar el artículo correspondiente al índice
+  items.splice(index, 1);
+
+  // Actualizar el local storage y volver a mostrar los elementos del carrito
+  localStorage.setItem("cart", JSON.stringify(items));
+  CartItems.innerHTML = ""; // Limpiar el contenido antes de volver a mostrar
+  displayCartItems();
+}
+
+displayCartItems();
---
+++ scripts/settings.js
@@ -0,0 +1,57 @@
+'use strict';
+
+
+/**
+ * Initializes the SDK and sets a global field with passed name for it the can
+ * be referred later
+ *
+ * @param {string} name Name by which the chat widget should be referred
+ */
+function initSdk(name) {
+    // Retry initialization later if WebSDK is not available yet
+    if (!document || !WebSDK) {
+        setTimeout(function () {
+            initSdk(name);
+        }, 2000);
+        return;
+    }
+
+    if (!name) {
+        name = 'Bots';          // Set default reference name to 'Bots'
+    }
+    var Bots;
+
+
+
+    setTimeout(function () {
+
+        var chatWidgetSettings = {
+            URI: 'oda-839c20ec3d9341c98f8c312ec749f5ed-da2.data.digitalassistant.oci.oraclecloud.com', // ODA URI, pass the hostname. Do not include the protocol (https://).
+
+            channelId: '0541d01b-e327-4472-a601-a93990251c9a', // Channel ID, available in channel settings in ODA UI
+
+//Add settings here
+            showConnectionStatus: true,
+            position: {bottom: '2px', right: '2px'},
+            initUserHiddenMessage: 'Hello',
+
+        };
+
+
+        Bots = new WebSDK(chatWidgetSettings);
+
+
+
+        Bots.connect();
+
+//Add Bots.setUserInputMessage('Order pizza'); here. Comment out for voice recognition.
+
+
+        // Create global object to refer Bots
+        window[name] = Bots;
+    }, 0);
+}
+
+
+
+
